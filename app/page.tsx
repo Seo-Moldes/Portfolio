5060d01ee59de6362e646bbdcf372146ef6983cb
Seo-Moldes <seomoldes@gmail.com>
2024-04-02 15:48:50 +0100

    page.tsx update

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-   <main>
-    Home
+   <main className="w-screen h-screen relative">
+    <div className="flex items-center w-full h-full bg-cover bg-center" style={{backgroundImage: "url(/main-bg.webp)"}}>
+
+    </div>
    </main>
   );
 }
