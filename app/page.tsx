acd9e12c7411f57b68f07f5a3cbef713a5a87f71
Seo-Moldes <seomoldes@gmail.com>
2024-04-03 12:07:17 +0100

    images, i18next library

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="w-screen h-screen relative">
-      <div className="flex items-center w-full h-full bg-cover bg-center" style={{ backgroundImage: "url(background4.jpg)" }}>
+      <div className="flex items-center w-full h-full bg-cover bg-center" style={{ backgroundImage: "url(darkSouls.gif)" }}>
         <div className="pl-20 md:pl-40 pb-56 md:pb-20 flex flex-col gap-5 z-[10] max-w-[750px]">
           <h1 className="text-[50px] text-white font-semibold">
             Make Anything possible with
@@ -16,6 +16,10 @@ export default function Home() {
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente molestiae esse iure inventore repellendus molestias a perspiciatis. Quia, sit libero soluta assumenda quasi non, magnam, aut aliquid ex quis accusamus!
           </p>
           <div className="flex-col md:flex-row hidden md:flex">
+          <Link href="/my-skills" className="rounded-[20px] group bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px] ">
+            Learn More
+          </Link>
+
           <Link href="/my-skills" className="rounded-[20px] group bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px] ">
             Learn More
           </Link>
